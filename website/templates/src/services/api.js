324007b69921a3b0bf4cd5b1d12d21768a407db8
Sean Reed <reedse.brocku@gmail.com>
2025-04-25 21:19:37 -0400

    update frontend api to use backend url

--- website/templates/src/services/api.js
+++ website/templates/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL of the Flask server (ensure it matches your backend's running address)
-const API_URL = 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL;
 
 // Create an Axios instance with default configurations
 const api = axios.create({
